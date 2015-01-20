30e47b4bfd42c18044f08dca7a5c2fe56020d65c
Raphael Jolly <rjolly@users.sourceforge.net>
2015-01-20 19:01:02 +0100

    empty

---
+++ src/main/java/linoleum/Browser.java
@@ -0,0 +1,117 @@
+package linoleum;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import javax.swing.JInternalFrame;
+
+public class Browser extends javax.swing.JInternalFrame {
+
+	public static class Application implements linoleum.application.Application {
+		public String getName() {
+			return Browser.class.getSimpleName();
+		}
+
+		public JInternalFrame open(final URI uri) {
+			return new Browser(uri);
+		}
+	}
+
+	public Browser(final URI uri) {
+		initComponents();
+		if (uri != null) try {
+			htmlPane1.linkActivated(uri.toURL());
+		} catch (MalformedURLException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	private void open(final String str) {
+		try {
+			htmlPane1.linkActivated(new URL(str));
+		} catch (MalformedURLException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
+
+                htmlPane1 = new linoleum.HtmlPane();
+                jPanel1 = new javax.swing.JPanel();
+                jTextField1 = new javax.swing.JTextField();
+                jButton1 = new javax.swing.JButton();
+
+                setClosable(true);
+                setIconifiable(true);
+                setMaximizable(true);
+                setResizable(true);
+                setTitle("Browser");
+
+                jTextField1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jTextField1ActionPerformed(evt);
+                        }
+                });
+
+                jButton1.setText("Go");
+                jButton1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jButton1ActionPerformed(evt);
+                        }
+                });
+
+                javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+                jPanel1.setLayout(jPanel1Layout);
+                jPanel1Layout.setHorizontalGroup(
+                        jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jTextField1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1)
+                                .addContainerGap())
+                );
+                jPanel1Layout.setVerticalGroup(
+                        jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jButton1)))
+                );
+
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(htmlPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(htmlPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE))
+                );
+
+                pack();
+        }// </editor-fold>//GEN-END:initComponents
+
+        private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+		open(jTextField1.getText());
+        }//GEN-LAST:event_jButton1ActionPerformed
+
+        private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+		open(evt.getActionCommand());
+        }//GEN-LAST:event_jTextField1ActionPerformed
+
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private linoleum.HtmlPane htmlPane1;
+        private javax.swing.JButton jButton1;
+        private javax.swing.JPanel jPanel1;
+        private javax.swing.JTextField jTextField1;
+        // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/java/linoleum/HtmlPane.java
@@ -0,0 +1,107 @@
+package linoleum;
+
+import java.awt.Container;
+import java.awt.Cursor;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
+import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Document;
+
+public class HtmlPane extends JScrollPane implements HyperlinkListener {
+
+    JEditorPane html;
+
+    @SuppressWarnings("LeakingThisInConstructor")
+    public HtmlPane() {
+        try {
+            URL url = getClass().getResource("blank.html");
+            html = new JEditorPane(url);
+            html.setEditable(false);
+            html.addHyperlinkListener(this);
+            html.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES,
+                    Boolean.TRUE);
+            JViewport vp = getViewport();
+            vp.add(html);
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e);
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+
+    /**
+     * Notification of a change relative to a
+     * hyperlink.
+     */
+    public void hyperlinkUpdate(HyperlinkEvent e) {
+        if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+            linkActivated(e.getURL());
+        }
+    }
+
+    /**
+     * Follows the reference in an
+     * link.  The given url is the requested reference.
+     * By default this calls <a href="#setPage">setPage</a>,
+     * and if an exception is thrown the original previous
+     * document is restored and a beep sounded.  If an
+     * attempt was made to follow a link, but it represented
+     * a malformed url, this method will be called with a
+     * null argument.
+     *
+     * @param u the URL to follow
+     */
+    protected void linkActivated(URL u) {
+        Cursor c = html.getCursor();
+        Cursor waitCursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
+        html.setCursor(waitCursor);
+        SwingUtilities.invokeLater(new PageLoader(u, c));
+    }
+
+
+    /**
+     * temporary class that loads synchronously (although
+     * later than the request so that a cursor change
+     * can be done).
+     */
+    class PageLoader implements Runnable {
+
+        PageLoader(URL u, Cursor c) {
+            url = u;
+            cursor = c;
+        }
+
+        public void run() {
+            if (url == null) {
+                // restore the original cursor
+                html.setCursor(cursor);
+
+                // PENDING(prinz) remove this hack when
+                // automatic validation is activated.
+                Container parent = html.getParent();
+                parent.repaint();
+            } else {
+                Document doc = html.getDocument();
+                try {
+                    html.setPage(url);
+                } catch (IOException ioe) {
+                    html.setDocument(doc);
+                    getToolkit().beep();
+                } finally {
+                    // schedule the cursor to revert after
+                    // the paint has happended.
+                    url = null;
+                    SwingUtilities.invokeLater(this);
+                }
+            }
+        }
+        URL url;
+        Cursor cursor;
+    }
+}
