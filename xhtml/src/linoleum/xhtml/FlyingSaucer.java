b7db5220d87653e97284c5232fd0bb32156ac637
Raphael Jolly <rjolly@users.sourceforge.net>
2017-01-20 11:51:59 +0100

    empty

---
+++ xhtml/src/linoleum/xhtml/FlyingSaucer.java
@@ -0,0 +1,75 @@
+package linoleum.xhtml;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.ImageIcon;
+import linoleum.application.Frame;
+
+public class FlyingSaucer extends Frame {
+	public FlyingSaucer() {
+		this(null);
+	}
+
+	public FlyingSaucer(final Frame parent) {
+		super(parent);
+		initComponents();
+		setIcon(new ImageIcon(getClass().getResource("/toolbarButtonGraphics/development/WebComponent24.gif")));
+		setMimeType("text/html:application/xhtml+xml");
+	}
+
+	@Override
+	public Frame getFrame(final Frame parent) {
+		return new FlyingSaucer(parent);
+	}
+
+	@Override
+	public void setURI(final URI uri) {
+		xHTMLPanel1.setDocument(uri.toString());
+	}
+
+	@Override
+	public URI getURI() {
+		final String str = xHTMLPanel1.getSharedContext().getBaseURL();
+		if (str != null) try {
+			return new URI(str);
+		} catch (final URISyntaxException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+        // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+        private void initComponents() {
+
+                jScrollPane1 = new javax.swing.JScrollPane();
+                xHTMLPanel1 = new org.xhtmlrenderer.simple.XHTMLPanel();
+
+                setClosable(true);
+                setIconifiable(true);
+                setMaximizable(true);
+                setResizable(true);
+                setTitle("Flying Saucer");
+
+                jScrollPane1.setViewportView(xHTMLPanel1);
+
+                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+                getContentPane().setLayout(layout);
+                layout.setHorizontalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 394, Short.MAX_VALUE)
+                );
+                layout.setVerticalGroup(
+                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE)
+                );
+
+                pack();
+        }// </editor-fold>//GEN-END:initComponents
+
+
+        // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JScrollPane jScrollPane1;
+        private org.xhtmlrenderer.simple.XHTMLPanel xHTMLPanel1;
+        // End of variables declaration//GEN-END:variables
+}
