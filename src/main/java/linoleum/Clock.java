8d1b8bdb3088364bef0c66ff53bb195b23a1f7bc
Raphael Jolly <rjolly@users.sourceforge.net>
2017-02-09 16:37:44 +0100

    empty

--- src/main/java/linoleum/Clock.java
+++ src/main/java/linoleum/Clock.java
@@ -1,24 +1,97 @@
 package linoleum;
 
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
+import java.util.prefs.Preferences;
 import linoleum.application.Frame;
 
 public class Clock extends Frame {
+	private final Preferences prefs = Preferences.userNodeForPackage(getClass());
 
 	public Clock() {
 		initComponents();
+		prefs.addPreferenceChangeListener(new PreferenceChangeListener() {
+			@Override
+			public void preferenceChange(final PreferenceChangeEvent evt) {
+				if (evt.getKey().equals(getKey("analog"))) {
+					refresh();
+				}
+			}
+		});
+		refresh();
+	}
+
+	private void refresh() {
+		clockPanel1.setAnalog(prefs.getBoolean(getKey("analog"), false));
+		pack();
+	}
+
+	@Override
+	public void init() {
+		getApplicationManager().addOptionPanel(optionPanel1);
+	}
+
+	@Override
+	public void load() {
+		jRadioButton1.setSelected(prefs.getBoolean(getKey("analog"), false));
+	}
+
+	@Override
+	public void save() {
+		prefs.putBoolean(getKey("analog"), jRadioButton1.isSelected());
 	}
 
 	@SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                optionPanel1 = new linoleum.application.OptionPanel();
+                jRadioButton1 = new javax.swing.JRadioButton();
+                jRadioButton2 = new javax.swing.JRadioButton();
+                buttonGroup1 = new javax.swing.ButtonGroup();
                 clockPanel1 = new linoleum.ClockPanel();
+
+                optionPanel1.setFrame(this);
+
+                buttonGroup1.add(jRadioButton1);
+                jRadioButton1.setText("Analog");
+                jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jRadioButton1ActionPerformed(evt);
+                        }
+                });
+
+                buttonGroup1.add(jRadioButton2);
+                jRadioButton2.setText("Digital");
+                jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jRadioButton2ActionPerformed(evt);
+                        }
+                });
+
+                javax.swing.GroupLayout optionPanel1Layout = new javax.swing.GroupLayout(optionPanel1);
+                optionPanel1.setLayout(optionPanel1Layout);
+                optionPanel1Layout.setHorizontalGroup(
+                        optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(optionPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jRadioButton1)
+                                        .addComponent(jRadioButton2))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
+                optionPanel1Layout.setVerticalGroup(
+                        optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(optionPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jRadioButton1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jRadioButton2)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                );
 
                 setClosable(true);
                 setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
-                setIconifiable(true);
-                setMaximizable(true);
-                setResizable(true);
                 setTitle("Clock");
                 addComponentListener(new java.awt.event.ComponentAdapter() {
                         public void componentShown(java.awt.event.ComponentEvent evt) {
@@ -28,17 +101,7 @@ public class Clock extends Frame {
                                 formComponentHidden(evt);
                         }
                 });
-
-                javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-                getContentPane().setLayout(layout);
-                layout.setHorizontalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(clockPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
-                );
-                layout.setVerticalGroup(
-                        layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(clockPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE)
-                );
+                getContentPane().add(clockPanel1, java.awt.BorderLayout.CENTER);
 
                 pack();
         }// </editor-fold>//GEN-END:initComponents
@@ -51,7 +114,19 @@ public class Clock extends Frame {
 		clockPanel1.start();
         }//GEN-LAST:event_formComponentShown
 
+        private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+		optionPanel1.setDirty(true);
+        }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+        private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+		optionPanel1.setDirty(true);
+        }//GEN-LAST:event_jRadioButton2ActionPerformed
+
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.ButtonGroup buttonGroup1;
         private linoleum.ClockPanel clockPanel1;
+        private javax.swing.JRadioButton jRadioButton1;
+        private javax.swing.JRadioButton jRadioButton2;
+        private linoleum.application.OptionPanel optionPanel1;
         // End of variables declaration//GEN-END:variables
 }
--- src/main/java/linoleum/ClockPanel.java
+++ src/main/java/linoleum/ClockPanel.java
@@ -1,189 +1,157 @@
 package linoleum;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JPanel;
 
-public class ClockPanel extends JPanel implements Runnable {
+public class ClockPanel extends JPanel {
+	private Timer timer; // The thread that displays clock
+	private final SimpleDateFormat sf = new SimpleDateFormat("s");
+	private final SimpleDateFormat mf = new SimpleDateFormat("m");
+	private final SimpleDateFormat hf = new SimpleDateFormat("h");
+	private final SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy", Locale.ENGLISH); // Formats the date displayed
+	private final Font clockFaceFont = new Font("Serif", Font.PLAIN, 14); // Font for number display on clock
+	private Date currentDate; // Used to get date to display
+	private Color handColor = Color.blue; // Color of main hands and dial
+	private Color numberColor = Color.darkGray; // Color of second hand and numbers
+	private int xcenter = 55, ycenter = 55; // Center position
+	private int width = 188;
+	private int height = 17;
+	private boolean analog;
 
-    private volatile Thread timer;       // The thread that displays clock
-    private int lastxs, lastys, lastxm,
-            lastym, lastxh, lastyh;  // Dimensions used to draw hands
-    private SimpleDateFormat formatter;  // Formats the date displayed
-    private String lastdate;             // String to hold date displayed
-    private Font clockFaceFont;          // Font for number display on clock
-    private Date currentDate;            // Used to get date to display
-    private Color handColor;             // Color of main hands and dial
-    private Color numberColor;           // Color of second hand and numbers
-    private int xcenter = 80, ycenter = 55; // Center position
+	public ClockPanel() {
+		try {
+			setBackground(new Color(Integer.parseInt(getParameter("bgcolor"), 16)));
+		} catch (NullPointerException e) {
+		} catch (NumberFormatException e) {
+		}
+		try {
+			handColor = new Color(Integer.parseInt(getParameter("fgcolor1"), 16));
+		} catch (NullPointerException e) {
+		} catch (NumberFormatException e) {
+		}
+		try {
+			numberColor = new Color(Integer.parseInt(getParameter("fgcolor2"), 16));
+		} catch (NullPointerException e) {
+		} catch (NumberFormatException e) {
+		}
+	}
 
-    public ClockPanel() {
-        lastxs = lastys = lastxm = lastym = lastxh = lastyh = 0;
-        formatter = new SimpleDateFormat("EEE MMM dd hh:mm:ss yyyy",
-                Locale.getDefault());
-        currentDate = new Date();
-        lastdate = formatter.format(currentDate);
-        clockFaceFont = new Font("Serif", Font.PLAIN, 14);
-        handColor = Color.blue;
-        numberColor = Color.darkGray;
+	public void setAnalog(final boolean analog) {
+		this.analog = analog;
+	}
 
-        try {
-            setBackground(new Color(Integer.parseInt(getParameter("bgcolor"),
-                    16)));
-        } catch (NullPointerException e) {
-        } catch (NumberFormatException e) {
-        }
-        try {
-            handColor = new Color(Integer.parseInt(getParameter("fgcolor1"),
-                    16));
-        } catch (NullPointerException e) {
-        } catch (NumberFormatException e) {
-        }
-        try {
-            numberColor = new Color(Integer.parseInt(getParameter("fgcolor2"),
-                    16));
-        } catch (NullPointerException e) {
-        } catch (NumberFormatException e) {
-        }
-//        resize(300, 300);              // Set clock window size
-    }
+	@Override
+	public Dimension getPreferredSize() {
+		return analog?new Dimension(110, 110):new Dimension(width + 10, height + 10); // Set clock window size
+	}
 
-    /**
-     * Paint is the main part of the program
-     */
-    @Override
-    public void paintComponent(Graphics g) {
-        int xh, yh, xm, ym, xs, ys;
-        int s = 0, m = 10, h = 10;
-        String today;
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setFont(clockFaceFont);
+		currentDate = new Date();
 
-	super.paintComponent(g);
+		if (analog) {
+			int xh, yh, xm, ym, xs, ys;
+			int s = 0, m = 10, h = 10;
 
-	g.setFont(clockFaceFont);
-        // Draw the circle and numbers
-        g.setColor(handColor);
-        g.drawArc(xcenter - 50, ycenter - 50, 100, 100, 0, 360);
-        g.setColor(numberColor);
-        g.drawString("9", xcenter - 45, ycenter + 3);
-        g.drawString("3", xcenter + 40, ycenter + 3);
-        g.drawString("12", xcenter - 5, ycenter - 37);
-        g.drawString("6", xcenter - 3, ycenter + 45);
+			// Draw the circle and numbers
+			g.setColor(handColor);
+			g.drawArc(xcenter - 50, ycenter - 50, 100, 100, 0, 360);
+			g.setColor(numberColor);
+			g.drawString("9", xcenter - 45, ycenter + 3);
+			g.drawString("3", xcenter + 40, ycenter + 3);
+			g.drawString("12", xcenter - 5, ycenter - 37);
+			g.drawString("6", xcenter - 3, ycenter + 45);
 
-        currentDate = new Date();
+			try {
+				s = Integer.parseInt(sf.format(currentDate));
+			} catch (NumberFormatException n) {
+			}
+			try {
+				m = Integer.parseInt(mf.format(currentDate));
+			} catch (NumberFormatException n) {
+			}
+			try {
+				h = Integer.parseInt(hf.format(currentDate));
+			} catch (NumberFormatException n) {
+			}
 
-        formatter.applyPattern("s");
-        try {
-            s = Integer.parseInt(formatter.format(currentDate));
-        } catch (NumberFormatException n) {
-            s = 0;
-        }
-        formatter.applyPattern("m");
-        try {
-            m = Integer.parseInt(formatter.format(currentDate));
-        } catch (NumberFormatException n) {
-            m = 10;
-        }
-        formatter.applyPattern("h");
-        try {
-            h = Integer.parseInt(formatter.format(currentDate));
-        } catch (NumberFormatException n) {
-            h = 10;
-        }
+			// Set position of the ends of the hands
+			xs = (int) (Math.cos(s * Math.PI / 30 - Math.PI / 2) * 45 + xcenter);
+			ys = (int) (Math.sin(s * Math.PI / 30 - Math.PI / 2) * 45 + ycenter);
+			xm = (int) (Math.cos(m * Math.PI / 30 - Math.PI / 2) * 40 + xcenter);
+			ym = (int) (Math.sin(m * Math.PI / 30 - Math.PI / 2) * 40 + ycenter);
+			xh = (int) (Math.cos((h * 30 + m / 2) * Math.PI / 180 - Math.PI / 2)
+					* 30
+					+ xcenter);
+			yh = (int) (Math.sin((h * 30 + m / 2) * Math.PI / 180 - Math.PI / 2)
+					* 30
+					+ ycenter);
 
-        // Set position of the ends of the hands
-        xs = (int) (Math.cos(s * Math.PI / 30 - Math.PI / 2) * 45 + xcenter);
-        ys = (int) (Math.sin(s * Math.PI / 30 - Math.PI / 2) * 45 + ycenter);
-        xm = (int) (Math.cos(m * Math.PI / 30 - Math.PI / 2) * 40 + xcenter);
-        ym = (int) (Math.sin(m * Math.PI / 30 - Math.PI / 2) * 40 + ycenter);
-        xh = (int) (Math.cos((h * 30 + m / 2) * Math.PI / 180 - Math.PI / 2)
-                * 30
-                + xcenter);
-        yh = (int) (Math.sin((h * 30 + m / 2) * Math.PI / 180 - Math.PI / 2)
-                * 30
-                + ycenter);
+			// Draw hands
+			g.setColor(numberColor);
+			g.drawLine(xcenter, ycenter, xs, ys);
+			g.setColor(handColor);
+			g.drawLine(xcenter, ycenter - 1, xm, ym);
+			g.drawLine(xcenter - 1, ycenter, xm, ym);
+			g.drawLine(xcenter, ycenter - 1, xh, yh);
+			g.drawLine(xcenter - 1, ycenter, xh, yh);
+		} else {
+			// Get the date to print at the bottom
+			final String today = formatter.format(currentDate);
 
-        // Get the date to print at the bottom
-        formatter.applyPattern("EEE MMM dd HH:mm:ss yyyy");
-        today = formatter.format(currentDate);
+			final FontMetrics fm = g.getFontMetrics();
+			final int ascent = fm.getAscent();
+			width = fm.stringWidth(today);
+			height = fm.getHeight();
 
-        g.setFont(clockFaceFont);
-        // Erase if necessary
-        g.setColor(getBackground());
-        if (xs != lastxs || ys != lastys) {
-            g.drawLine(xcenter, ycenter, lastxs, lastys);
-            g.drawString(lastdate, 5, 125);
-        }
-        if (xm != lastxm || ym != lastym) {
-            g.drawLine(xcenter, ycenter - 1, lastxm, lastym);
-            g.drawLine(xcenter - 1, ycenter, lastxm, lastym);
-        }
-        if (xh != lastxh || yh != lastyh) {
-            g.drawLine(xcenter, ycenter - 1, lastxh, lastyh);
-            g.drawLine(xcenter - 1, ycenter, lastxh, lastyh);
-        }
+			// Draw date
+			g.setColor(numberColor);
+			g.drawString(today, 5, ascent + 5);
+		}
+	}
 
-        // Draw date and hands
-        g.setColor(numberColor);
-        g.drawString(today, 5, 125);
-        g.drawLine(xcenter, ycenter, xs, ys);
-        g.setColor(handColor);
-        g.drawLine(xcenter, ycenter - 1, xm, ym);
-        g.drawLine(xcenter - 1, ycenter, xm, ym);
-        g.drawLine(xcenter, ycenter - 1, xh, yh);
-        g.drawLine(xcenter - 1, ycenter, xh, yh);
-        lastxs = xs;
-        lastys = ys;
-        lastxm = xm;
-        lastym = ym;
-        lastxh = xh;
-        lastyh = yh;
-        lastdate = today;
-        currentDate = null;
-    }
+	public void start() {
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				repaint();
+			}
+		}, 0, 1000);
+	}
 
-    public void start() {
-        timer = new Thread(this);
-        timer.start();
-    }
+	public void stop() {
+		timer.cancel();
+	}
 
-    public void stop() {
-        timer = null;
-    }
+	public String getAppletInfo() {
+		return "Title: A Clock \n"
+				+ "Author: Rachel Gollub, 1995 \n"
+				+ "An analog clock.";
+	}
 
-    @Override
-    @SuppressWarnings("SleepWhileHoldingLock")
-    public void run() {
-        Thread me = Thread.currentThread();
-        while (timer == me) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-            }
-            repaint();
-        }
-    }
-
-    public String getAppletInfo() {
-        return "Title: A Clock \n"
-                + "Author: Rachel Gollub, 1995 \n"
-                + "An analog clock.";
-    }
-
-    public String[][] getParameterInfo() {
-        String[][] info = {
-            { "bgcolor", "hexadecimal RGB number",
-                "The background color. Default is the color of your browser." },
-            { "fgcolor1", "hexadecimal RGB number",
-                "The color of the hands and dial. Default is blue." },
-            { "fgcolor2", "hexadecimal RGB number",
-                "The color of the second hand and numbers. Default is dark gray." }
-        };
-        return info;
-    }
+	public String[][] getParameterInfo() {
+		String[][] info = {
+			{ "bgcolor", "hexadecimal RGB number",
+				"The background color. Default is the color of your browser." },
+			{ "fgcolor1", "hexadecimal RGB number",
+				"The color of the hands and dial. Default is blue." },
+			{ "fgcolor2", "hexadecimal RGB number",
+				"The color of the second hand and numbers. Default is dark gray." }
+		};
+		return info;
+	}
 
 	private String getParameter(final String name) {
 		return null;
