d198038d244534b025d8c2db4217fbd24e2432f9
Raphael Jolly <rjolly@users.sourceforge.net>
2018-01-26 11:52:06 +0100

    empty

--- console/build.js
+++ console/build.js
@@ -1,11 +1,19 @@
 mkdir("build");
 mkdir("build/classes");
+mkdir("build/sources");
+mkdir("build/javadoc");
 
 javac("src", "build/classes");
 copy("res", "build/classes");
+copy("src", "build/sources");
+copy("res/linoleum", "build/sources/linoleum");
+javadoc("src", "build/javadoc");
 
 mkdir("dist");
 var name = "console";
 jar("dist/" + name + ".jar", "build/classes");
+jar("dist/" + name + "-source.jar", "build/sources");
+jar("dist/" + name + "-javadoc.jar", "build/javadoc");
+cp("pom.xml", "dist/" + name + ".pom")
 
 publish("dist")
