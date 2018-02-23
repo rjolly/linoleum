9c24f2093af00e3559607684956e710a974b79e4
Raphael Jolly <rjolly@users.sourceforge.net>
2018-02-23 21:18:18 +0100

    empty

--- pkg/src/linoleum/pkg/PackageManager.java
+++ pkg/src/linoleum/pkg/PackageManager.java
@@ -49,20 +49,19 @@ public class PackageManager extends Frame {
 		}
 	}
 
-	private File[] installed() {
-		final SortedMap<String, File> map = new TreeMap<>();
+	private Package[] installed() {
+		final SortedMap<String, Package> map = new TreeMap<>();
 		for (final String str : System.getProperty("java.class.path").split(File.pathSeparator)) {
-			final File file = new File(str);
-			map.put(new Package(file).getName(), file);
+			final Package pkg = new Package(new File(str));
+			map.put(pkg.getName(), pkg);
 		}
-		return map.values().toArray(new File[0]);
+		return map.values().toArray(new Package[0]);
 	}
 
 	@Override
 	public void open() {
 		model.setRowCount(0);
-		for (final File file : installed()) {
-			final Package pkg = new Package(file);
+		for (final Package pkg : installed()) {
 			model.addRow(new Object[] {pkg.getName(), pkg.getVersion(), pkg.isSnapshot()});
 		}
 		final URI uri = getURI();
