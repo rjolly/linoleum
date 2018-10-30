da3b34df2e0e138cc20d476e162ac153f88799f5
Raphael Jolly <rjolly@users.sourceforge.net>
2018-10-30 10:56:20 +0100

    empty

--- bin/linoleum.bat
+++ bin/linoleum.bat
@@ -1,3 +1,3 @@
 @echo off
 set LINOLEUM_HOME=%~dp0..
-java -Dfile.encoding=UTF-8 -Djava.system.class.loader=linoleum.application.ClassLoader -jar %LINOLEUM_HOME%\linoleum.jar
+java -Dfile.encoding=UTF-8 -Djava.system.class.loader=linoleum.application.ClassLoader -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false -jar %LINOLEUM_HOME%\linoleum.jar
