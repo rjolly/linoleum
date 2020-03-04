ee12d43f4efa43f594e8f7e53c7c4f45fbff3632
Raphael Jolly <rjolly@users.sourceforge.net>
2020-03-04 15:10:50 +0100

    empty

--- src/main/java/linoleum/Background.java
+++ src/main/java/linoleum/Background.java
@@ -52,12 +52,18 @@ public class Background extends PreferenceSupport {
 	public void load() {
 		jTextField1.setText(getPref("image"));
 		model.setSelectedItem(getTheme());
+		jTextField2.setText(getPref("contents"));
 	}
 
 	@Override
 	public void save() {
 		putPref("image", jTextField1.getText());
 		putPref("theme", model.getSelectedItem());
+		putPref("contents", jTextField2.getText());
+	}
+
+	String getContents() {
+		return getPref("contents");
 	}
 
 	private Icon getImage() {
@@ -98,6 +104,8 @@ public class Background extends PreferenceSupport {
                 jButton1 = new javax.swing.JButton();
                 jLabel3 = new javax.swing.JLabel();
                 jComboBox1 = new javax.swing.JComboBox<>();
+                jTextField2 = new javax.swing.JTextField();
+                jLabel4 = new javax.swing.JLabel();
                 jLabel1 = new javax.swing.JLabel();
 
                 jLabel2.setText("Image :");
@@ -118,6 +126,8 @@ public class Background extends PreferenceSupport {
                         }
                 });
 
+                jLabel4.setText("Contents :");
+
                 javax.swing.GroupLayout optionPanel1Layout = new javax.swing.GroupLayout(optionPanel1);
                 optionPanel1.setLayout(optionPanel1Layout);
                 optionPanel1Layout.setHorizontalGroup(
@@ -126,14 +136,16 @@ public class Background extends PreferenceSupport {
                                 .addContainerGap()
                                 .addGroup(optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                         .addComponent(jLabel3)
-                                        .addComponent(jLabel2))
+                                        .addComponent(jLabel2)
+                                        .addComponent(jLabel4))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(optionPanel1Layout.createSequentialGroup()
                                                 .addComponent(jTextField1)
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                        .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jTextField2))
                                 .addContainerGap())
                 );
                 optionPanel1Layout.setVerticalGroup(
@@ -148,11 +160,16 @@ public class Background extends PreferenceSupport {
                                 .addGroup(optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(jLabel3)
                                         .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(optionPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel4))
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 );
 
                 setBackground(zero);
                 setOptionPanel(optionPanel1);
+                getContentPane().setLayout(new java.awt.BorderLayout());
 
                 jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
                 jLabel1.setIcon(getImage());
@@ -181,7 +198,9 @@ public class Background extends PreferenceSupport {
         private javax.swing.JLabel jLabel1;
         private javax.swing.JLabel jLabel2;
         private javax.swing.JLabel jLabel3;
+        private javax.swing.JLabel jLabel4;
         private javax.swing.JTextField jTextField1;
+        private javax.swing.JTextField jTextField2;
         private linoleum.application.OptionPanel optionPanel1;
         // End of variables declaration//GEN-END:variables
 }
