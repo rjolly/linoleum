9bbe484815a59be90125b088b0ad947a7b0ab0d9
Raphael Jolly <rjolly@users.sourceforge.net>
2024-01-30 15:32:08 +0100

    empty

--- bin/linoleum-headless.bat
+++ bin/linoleum-headless.bat
@@ -1,3 +1,3 @@
 @echo off
 set LINOLEUM_HOME=%~dp0..
-jrunscript -J-Dfile.encoding=UTF-8 -J-Djava.system.class.loader=linoleum.application.ClassLoader -J-Djava.class.path=%JAVA_HOME%\lib\tools.jar;%LINOLEUM_HOME%\linoleum.jar -f %LINOLEUM_HOME%\init.js %*
+jrunscript -J-Dfile.encoding=UTF-8 -J-Djava.system.class.loader=linoleum.application.ClassLoader -J--add-opens -Jjdk.jdeps/com.sun.tools.javap=ALL-UNNAMED -J--add-opens -Jjdk.jconsole/sun.tools.jconsole=ALL-UNNAMED -J-Djava.class.path=%LINOLEUM_HOME%\linoleum.jar -f %LINOLEUM_HOME%\init.js %*
