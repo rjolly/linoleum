7b5605d1792da17712a9557d7ab57ead537f75a2
Raphael Jolly <rjolly@users.sourceforge.net>
2024-02-01 14:01:23 +0100

    empty

--- bin/linoleum-headless.bat
+++ bin/linoleum-headless.bat
@@ -1,3 +1,3 @@
 @echo off
 set LINOLEUM_HOME=%~dp0..
-jrunscript -J-Dfile.encoding=UTF-8 -J-Djava.system.class.loader=linoleum.application.ClassLoader -J--add-opens -Jjdk.jdeps/com.sun.tools.javap=ALL-UNNAMED -J--add-opens -Jjdk.jconsole/sun.tools.jconsole=ALL-UNNAMED -J-Djava.class.path=%LINOLEUM_HOME%\linoleum.jar -f %LINOLEUM_HOME%\init.js %*
+jrunscript -J-Dfile.encoding=UTF-8 -J-Djava.system.class.loader=linoleum.application.ClassLoader -J--add-opens -Jjdk.jdeps/com.sun.tools.javap=ALL-UNNAMED -J--add-opens -Jjdk.jconsole/sun.tools.jconsole=ALL-UNNAMED -J-Djava.class.path=%LINOLEUM_HOME%\linoleum.jar;%LINOLEUM_HOME%\lib\nashorn-core-15.4.jar;%LINOLEUM_HOME%\lib\asm-7.3.1.jar";%LINOLEUM_HOME%\lib\asm-analysis-7.3.1.jar";%LINOLEUM_HOME%\lib\asm-commons-7.3.1.jar;%LINOLEUM_HOME%\lib\asm-tree-7.3.1.jar;%LINOLEUM_HOME%\lib\asm-util-7.3.1.jar -f %LINOLEUM_HOME%\init.js %*
